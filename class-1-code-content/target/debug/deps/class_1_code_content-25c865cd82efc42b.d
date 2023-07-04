1f8cf39da8c7297260807a20e85d7772fb03bf7a
VN The Boss Baby <93357243+QuDaMyker@users.noreply.github.com>
2023-07-04 11:04:42 +0700

    day 01

---
+++ class-1-code-content/target/debug/deps/class_1_code_content-25c865cd82efc42b.d
@@ -0,0 +1,5 @@
+E:\GitHub\Rust-Bootcamp-2023\class-1-code-content\target\debug\deps\class_1_code_content-25c865cd82efc42b.rmeta: src\main.rs
+
+E:\GitHub\Rust-Bootcamp-2023\class-1-code-content\target\debug\deps\class_1_code_content-25c865cd82efc42b.d: src\main.rs
+
+src\main.rs:
